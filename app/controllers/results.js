4d4a2051985f6b790069f270587ac1491332ea24
Giovanni Lenguito <giovanni16.gl@gmail.com>
2017-03-11 23:18:13 +0000

    - Gets winner and displays it in results

--- app/controllers/results.js
+++ app/controllers/results.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  application: Ember.inject.controller(),
+  runnersUp: [],
+
+  sortDesc: ['numberOfVotes:desc'],
+  sortedCandidates: Ember.computed.sort('application.candidates', 'sortDesc'),
+  getTopVoted: function(){
+    debugger;
+    return this.get("sortedCandidates.firstObject");
+  }.property("sortedCandidates")
 });
