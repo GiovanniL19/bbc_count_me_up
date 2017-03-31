291b511a14e703f16a0b76646ee79bcf132e105e
Giovanni Lenguito <giovanni16.gl@gmail.com>
2017-03-31 17:04:13 +0100

    - Implements database sync - Displays multiple winners (if necessary) - Shows ties

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,13 @@
 import PouchDB from 'pouchdb';
 import { Adapter } from 'ember-pouch';
 
+//var remote = new PouchDB('http://localhost:5984/count-me-up');
 var db = new PouchDB('count-me-up-pouch');
+//
+// db.sync(remote, {
+//   live: true,
+//   retry: true
+// });
 
 export default Adapter.extend({
   db: db
---
+++ tests/unit/helpers/tie-test.js
@@ -0,0 +1,12 @@
+
+import { tie } from 'count-me-up/helpers/tie';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | tie');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = tie([42]);
+  assert.ok(result);
+});
+
